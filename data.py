a5336f1133b2c08a6dd54446e8e9c630a49011ad
Kaelo Shuping <122022296+kaeloshuping@users.noreply.github.com>
2023-06-06 15:07:17 +0200

    initial commit

---
+++ data.py
@@ -0,0 +1,23 @@
+
+printers = {
+    "B&W": {
+        "014": "VINTAGE",
+        "017": "VENICE",
+        "243": "NANA",
+        "246": "MUFASA",
+        "247": "TELLYTUBBY",
+        "248": "SWEETNESS",
+        "341": "MJEKEJEKE",
+        },
+    "COLOUR": {
+        "828": "SPONGEBOB (C3070)",
+        },
+}
+
+finishing_equipment = ["SD UNIT (NAGEL)", "GELATINE 1", "GELATINE 2", "GPM PERFORATOR", "LAMINATOR", "PUR GLUE BINDER"]
+
+columns = {
+    "current": 3,
+    "previous": 5,
+    "difference": 6,
+}
